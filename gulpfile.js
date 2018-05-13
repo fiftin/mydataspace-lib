de9b6ae0e5210aa736cb2096a03786fc90b2307f
Denis Gukov <denguk@gmail.com>
2018-05-13 23:55:48 +0500

    add scss

--- gulpfile.js
+++ gulpfile.js
@@ -1,29 +1,23 @@
 var gulp = require('gulp');
-var bower = require('gulp-bower');
 var runSequence = require('run-sequence');
 var del = require('del');
 var concat = require('gulp-concat');
 
 
-gulp.task('bower', function() {
-  return bower();
-});
-
 gulp.task('vendor:bootstrap:css', function() {
   return gulp.src(
-    'bower_components/bootstrap-sass/assets/stylesheets/**/*'
+    'node_modules/bootstrap-sass/assets/stylesheets/**/*'
   ).pipe(gulp.dest('scss/bootstrap'));
 });
 
 gulp.task('vendor:bootstrap', ['vendor:bootstrap:css']);
 
 gulp.task('vendor:clean', function() {
-  return del(['vendor/**/*']);
+  return del(['scss/**/*']);
 });
 
 gulp.task('default', function() {
   runSequence(
-    'bower',
     'vendor:clean',
     [
       'vendor:bootstrap'
